e41c75e879fe26f4443f74eebf37ecd187f49b0c
Saifeddine Mahjoub <smahjoub@smahjoub.localhost.com>
2019-05-01 15:37:50 +0200

    Remove hard coded file path

--- src/main/scala/com/smahjoub/wordcounter/App.scala
+++ src/main/scala/com/smahjoub/wordcounter/App.scala
@@ -8,14 +8,16 @@ object App {
   def main(args: Array[String]): Unit = {
     val sparkConf = new SparkConf().setAppName("Word counter app")
     val sc = new SparkContext(sparkConf)
+    val sourceFile = args(0)
+    val targetFile = args(1)
 
-    val textFile = sc.textFile("file:///E:/Documents/spark-examples/spark-input.txt")
+    val textFile = sc.textFile(sourceFile)
 
     val tokenizedTextFile = textFile.flatMap(line => line.split(" "))
     val countPrep = tokenizedTextFile.map(word => (word, 1))
 
     val counts = countPrep.reduceByKey((accValue, newValue) =>  accValue + newValue)
     val sortedCounts = counts.sortBy(kvPair => kvPair._2, false)
-    sortedCounts.saveAsTextFile("file:///E:/Documents/spark-examples//spark-output")
+    sortedCounts.saveAsTextFile(targetFile)
   }
 }
